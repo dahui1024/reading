500977a480ad15b12c1a2da9960d3dc45ce1e209
wanghui <adan_wang@iyouqu.net>
2017-11-21 14:19:33 +0800

    添加日志模块

--- quartz/src/main/java/com/bbcow/task/WordTask.java
+++ quartz/src/main/java/com/bbcow/task/WordTask.java
@@ -10,6 +10,8 @@ import org.apache.commons.lang3.StringUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -23,75 +25,81 @@ import java.util.TreeMap;
  */
 @Component
 public class WordTask {
+    Logger logger = LoggerFactory.getLogger(WordTask.class);
+
     @Autowired
     ScoreService scoreService;
     @Autowired
     BookService bookService;
     boolean isRunning = false;
 
-    @Scheduled(cron = "0 0/10 * * * ?")
+    @Scheduled(cron = "0 0/5 * * * ?")
     public void pageScoreTask(){
         if (isRunning){
             return;
         }
         isRunning = true;
 
-        List<BookUrl> bookUrls = bookService.getFinishBookChapter();
-        bookUrls.forEach(bookUrl -> {
-            List<BookChapter> bookChapters = bookService.getBookChapters(bookUrl.getReferenceKey());
+        try {
+            List<BookUrl> bookUrls = bookService.getFinishBookChapter();
+            bookUrls.forEach(bookUrl -> {
+                List<BookChapter> bookChapters = bookService.getBookChapters(bookUrl.getReferenceKey());
 
-            Map<String, BookWord> wordMap = new TreeMap<>();
+                Map<String, BookWord> wordMap = new TreeMap<>();
 
-            // 初始化一个AipNlp
-            AipNlp client = new AipNlp("10347588", "Yfm4Gpa4lzPGd8kh989DS8c3", "N2mYTHhK4USBbaI3ccT9zODghPTusNuY");
+                // 初始化一个AipNlp
+                AipNlp client = new AipNlp("10347588", "Yfm4Gpa4lzPGd8kh989DS8c3", "N2mYTHhK4USBbaI3ccT9zODghPTusNuY");
 
-            // 可选：设置网络连接参数
-            client.setConnectionTimeoutInMillis(2000);
-            client.setSocketTimeoutInMillis(60000);
+                // 可选：设置网络连接参数
+                client.setConnectionTimeoutInMillis(2000);
+                client.setSocketTimeoutInMillis(60000);
 
-            bookChapters.forEach(bookChapter -> {
-
-                if (bookChapter.getContent() == null){
-                    return;
-                }
-
-                JSONObject res = client.lexer(bookChapter.getContent());
-                try {
-                    JSONArray items = res.getJSONArray("items");
-                    for (int i = 0; i < items.length(); i++) {
-                        JSONObject item = items.getJSONObject(i);
-                        String word = StringUtils.trim(item.getString("item"));
-                        String ne = item.getString("ne");
-                        String pos = item.getString("pos");
-
-                        if (StringUtils.isNotBlank(word) && ("PER".equals(ne) || "n".equals(pos))){
-                            if (wordMap.containsKey(word)){
-                                BookWord bookWord = wordMap.get(word);
-                                bookWord.setCount(bookWord.getCount()+1);
-                            }else {
-                                BookWord bookWord = new BookWord();
-                                bookWord.setWord(word);
-                                bookWord.setCount(1);
-                                bookWord.setReferenceKey(bookChapter.getReferenceKey());
-                                if (StringUtils.isNotBlank(ne)){
-                                    bookWord.setTag(ne);
-                                }
-                                if (StringUtils.isNotBlank(pos)){
-                                    bookWord.setType(pos);
-                                }
-                                wordMap.put(word, bookWord);
-                            }
-
-                        }
+                bookChapters.forEach(bookChapter -> {
+                    if (bookChapter.getContent() == null){
+                        return;
                     }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            });
-            bookService.saveWords(wordMap.values());
-            bookService.finishChapterWord(bookUrl.getReferenceKey());
-        });
+                    JSONObject res = client.lexer(bookChapter.getContent());
 
-        isRunning = false;
+                    try {
+                        JSONArray items = res.getJSONArray("items");
+                        for (int i = 0; i < items.length(); i++) {
+                            JSONObject item = items.getJSONObject(i);
+                            String word = StringUtils.trim(item.getString("item"));
+                            String ne = item.getString("ne");
+                            String pos = item.getString("pos");
+
+                            if (StringUtils.isNotBlank(word) && ("PER".equals(ne) || "n".equals(pos))){
+                                if (wordMap.containsKey(word)){
+                                    BookWord bookWord = wordMap.get(word);
+                                    bookWord.setCount(bookWord.getCount()+1);
+                                }else {
+                                    BookWord bookWord = new BookWord();
+                                    bookWord.setWord(word);
+                                    bookWord.setCount(1);
+                                    bookWord.setReferenceKey(bookChapter.getReferenceKey());
+                                    if (StringUtils.isNotBlank(ne)){
+                                        bookWord.setTag(ne);
+                                    }
+                                    if (StringUtils.isNotBlank(pos)){
+                                        bookWord.setType(pos);
+                                    }
+                                    wordMap.put(word, bookWord);
+                                }
+
+                            }
+                        }
+                    } catch (JSONException e) {
+                        logger.info(e.getMessage(), e);
+                    }
+                });
+                bookService.saveWords(wordMap.values());
+                bookService.finishChapterWord(bookUrl.getReferenceKey());
+            });
+
+        } catch (Exception e){
+            logger.info(e.getMessage(), e);
+        } finally {
+            isRunning = false;
+        }
     }
 }
